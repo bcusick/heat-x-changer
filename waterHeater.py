68dd8eeb06278025621a14a8495da47f64b4f857
Bryan Cusick <bryancusick@gmail.com>
2019-01-29 13:27:56 -0800

    update with electric aux heat

--- waterHeater.py
+++ waterHeater.py
@@ -18,15 +18,15 @@ length1 =  199./1000 #all dims converted to meters
 width1 = 86./1000
 plateThk1 = 2.35/1000
 heatXA1 = .014 #m2
-numPlates1 = 10
-flowrateCoolant1 = 5.   #GPM
+numPlates1 = 20
+flowrateAF1 = 5   #GPM
 
 length2 =  199./1000 #all dims converted to meters
 width2 = 86./1000
 plateThk2 = 2.35/1000
 heatXA2 = .014 #m2
 numPlates2 = 30
-flowrateCoolant2 = 5.   #GPM
+flowrateAF2 = 5   #GPM
 
 #Duda Diesel B3-32DW
 length3 =  286./1000 #all dims converted to meters
@@ -34,36 +34,35 @@ width3 = 116./1000
 plateThk3 = 2.4/1000
 heatXA3 = .032 #m2
 numPlates3 = 20
-flowrateCoolant3 = 5.   #GPM
-
+flowrateAF3 = 5   #GPM
 
 #constants
 
-tempCoolant = -5 # Celsius, Webasto temp 78C, minimum
-tempFuel = 30  # Celsius
-consumeFuel = 0.37 #lb/HP/hr
-
+tempAF = 65 # Celsius, Webasto temp 78C, minimum
+tempWater = 35  # Celsius
+# consumeWater = 0.37 #lb/HP/hr
+waterTarget = 50
 
 ##fluid constants
-##coolant to 50-50 glycol/water
+##AF to 50-50 glycol/water
 
-k_Coolant = 0.415 # W/(m K), thermal conductivity
-C_Coolant = 3681.9 # J/(kg K), Specific Heat
-rho_Coolant = 1015.6 # kg/m3, Density
-mu_Coolant = 0.000744 # Pa s, Dynamic Viscosity
+k_AF = 0.415 # W/(m K), thermal conductivity
+C_AF = 3681.9 # J/(kg K), Specific Heat
+rho_AF = 1015.6 # kg/m3, Density
+mu_AF = 0.000744 # Pa s, Dynamic Viscosity
 
-k_Fuel = 0.6 # W/(m K), thermal conductivity, water
-C_Fuel = 4184.0 # J/(kg K), Specific Heat, water
-rho_Fuel = 1000. # kg/m3, Density, water
-mu_Fuel = 0.001 # Pa s, Dynamic Viscosity, water
+k_Water = 0.6 # W/(m K), thermal conductivity, water
+C_Water = 4184.0 # J/(kg K), Specific Heat, water
+rho_Water = 1000. # kg/m3, Density, water
+mu_Water = 0.001 # Pa s, Dynamic Viscosity, water
 
 #conversions
-consumeFuel /= 2.2 #kg/HP/hr
-consumeFuel /= 3600. #kg/HP/s
-consumeFuel /= rho_Fuel #m3/HP/s
+# consumeWater /= 2.2 #kg/HP/hr
+# consumeWater /= 3600. #kg/HP/s
+# consumeWater /= rho_Water #m3/HP/s
 
 
-def radCalc(numPlates, heatXA, length, width, plateThk, flowrateCoolant):
+def radCalc(numPlates, heatXA, length, width, plateThk, flowrateAF):
 
 
 
@@ -73,99 +72,107 @@ def radCalc(numPlates, heatXA, length, width, plateThk, flowrateCoolant):
 
 
 
-    areaCoolant = heatXA * numPlates
-    areaFuel =  heatXA * numPlates
+    areaAF = heatXA * numPlates
+    areaWater =  heatXA * numPlates
 
     areaXSec = plateThk * width
     perimeterXSec = 2. * (plateThk + width)
 
-    ## coolant calcs
-    flowrateCoolant = flowrateCoolant * 3.8 #LPM
-    flowrateCoolant = flowrateCoolant /60./1000. # convert to m3/s
-    massflowCoolant = flowrateCoolant *rho_Coolant
-    print massflowCoolant * C_Coolant
+    ## AF calcs
+    flowrateAF = flowrateAF * 3.8 #LPM
+    flowrateAF = flowrateAF /60./1000. # convert to m3/s
+    massflowAF = flowrateAF *rho_AF
+    # print (massflowAF * C_AF)
 
     ## fluids calcs
-    #Coolant
-    Dh_Coolant = 4. * areaXSec / perimeterXSec
-    coolantVelocity = flowrateCoolant/(numPlates / 2.)/(areaXSec)
-    reynoldsCoolant = fluids.core.Reynolds(D=Dh_Coolant, rho=rho_Coolant, V=coolantVelocity, mu=mu_Coolant)
-    prandltCoolant = fluids.core.Prandtl(Cp=C_Coolant , k=k_Coolant , mu=mu_Coolant, nu=None, rho=None, alpha=None)
-    if reynoldsCoolant<5000:
-        nusseltCoolant = ht.conv_internal.laminar_Q_const()
+    #AF
+    Dh_AF = 4. * areaXSec / perimeterXSec
+    AFVelocity = flowrateAF/(numPlates / 2.)/(areaXSec)
+    reynoldsAF = fluids.core.Reynolds(D=Dh_AF, rho=rho_AF, V=AFVelocity, mu=mu_AF)
+    prandltAF = fluids.core.Prandtl(Cp=C_AF , k=k_AF , mu=mu_AF, nu=None, rho=None, alpha=None)
+    if reynoldsAF<5000:
+        nusseltAF = ht.conv_internal.laminar_Q_const()
     else:
-        nusseltCoolant = ht.conv_internal.turbulent_Dittus_Boelter(Re=reynoldsCoolant, Pr=prandltCoolant, heating=False)
-    h_Coolant = nusseltCoolant * k_Coolant / Dh_Coolant
+        nusseltAF = ht.conv_internal.turbulent_Dittus_Boelter(Re=reynoldsAF, Pr=prandltAF, heating=False)
+    h_AF = nusseltAF * k_AF / Dh_AF
 
 
     #setup arrays for data.
     temp = []
     #power = []
-    rateFuel =[]
+    rateWater =[]
 
 
 
-    for flowrateFuel1 in float_range(.01, 0.5, .01):
+    for flowrateWater1 in float_range(2, 4, .1):
 
 
-        flowrateFuel = flowrateFuel1 * 3.8 #LPM
-        flowrateFuel = flowrateFuel /60./1000. # convert to m3/s
-        massflowFuel = flowrateFuel *rho_Fuel
+        # flowrateWater1 = flowrateWater1 * 3.8 #LPM
+        flowrateWater = flowrateWater1 /60./1000. # convert to m3/s
+        massflowWater = flowrateWater *rho_Water
 
 
 
 
         ## fluids calcs
-        #Fuel
-        Dh_Fuel = 4. * areaXSec / perimeterXSec
-        fuelVelocity = flowrateFuel/(numPlates / 2.)/(areaXSec)
-        reynoldsFuel = fluids.core.Reynolds(D=Dh_Fuel, rho=rho_Fuel, V=fuelVelocity, mu=mu_Fuel)
-        prandltFuel = fluids.core.Prandtl(Cp=C_Fuel , k=k_Fuel , mu=mu_Fuel, nu=None, rho=None, alpha=None)
-        if reynoldsFuel<5000:
-            nusseltFuel = ht.conv_internal.laminar_Q_const()
+        #Water
+        Dh_Water = 4. * areaXSec / perimeterXSec
+        WaterVelocity = flowrateWater/(numPlates / 2.)/(areaXSec)
+        reynoldsWater = fluids.core.Reynolds(D=Dh_Water, rho=rho_Water, V=WaterVelocity, mu=mu_Water)
+        prandltWater = fluids.core.Prandtl(Cp=C_Water , k=k_Water , mu=mu_Water, nu=None, rho=None, alpha=None)
+        if reynoldsWater<5000:
+            nusseltWater = ht.conv_internal.laminar_Q_const()
         else:
-            nusseltFuel = ht.conv_internal.turbulent_Dittus_Boelter(Re=reynoldsFuel, Pr=prandltFuel, heating=True)
-        #nusseltFuel = 10  # manual overide for laminare flow
+            nusseltWater = ht.conv_internal.turbulent_Dittus_Boelter(Re=reynoldsWater, Pr=prandltWater, heating=True)
+        #nusseltWater = 10  # manual overide for laminare flow
 
-        h_Fuel = nusseltFuel * k_Fuel / Dh_Fuel
+        h_Water = nusseltWater * k_Water / Dh_Water
 
 
 
-        UA = 1./(h_Coolant*areaCoolant) + 1./(h_Fuel*areaFuel)
+        UA = 1./(h_AF*areaAF) + 1./(h_Water*areaWater)
         UA = 1./UA
 
 
-        NTU = ht.hx.effectiveness_NTU_method(mh=massflowFuel, mc=massflowCoolant, Cph=C_Fuel, Cpc=C_Coolant, subtype='counterflow', Thi=tempFuel, Tho=None, Tci=tempCoolant, Tco=None, UA=UA)
-        #NTU = ht.hx.effectiveness_NTU_method(mh=massflowAir, mc=massflowCoolant, Cph=C_Air, Cpc=C_Coolant, subtype='crossflow', Thi=tempAir, Tho=None, Tci=tempCoolant, Tco=None, UA=UA)
+        NTU = ht.hx.effectiveness_NTU_method(mc=massflowWater, mh=massflowAF, Cpc=C_Water, Cph=C_AF, subtype='counterflow', Tci=tempWater, Tho=None, Thi=tempAF, Tco=None, UA=UA)
+        #NTU = ht.hx.effectiveness_NTU_method(mh=massflowAir, mc=massflowAF, Cph=C_Air, Cpc=C_AF, subtype='crossflow', Thi=tempAir, Tho=None, Tci=tempAF, Tco=None, UA=UA)
         #Power = (NTU['Q']/1000) /0.3/.75
-        tOut = (NTU['Q'])
-        #tOut = C_Fuel / tOut * (rho_Fuel/1000.) * 15 * 3.8 *10/60.  #sort of calulate time in mins. to heat 15gal water
+        Tco = (NTU['Tco'])
+        Tho = (NTU['Tho'])
+        Q = (NTU['Q'])
+        #tOut = C_Water / tOut * (rho_Water/1000.) * 15 * 3.8 *10/60.  #sort of calulate time in mins. to heat 15gal water
 
-        temp.append(tOut) #fill data arrays
-        rateFuel.append(flowrateFuel1)
+        temp.append(Tco) #fill data arrays
+        rateWater.append((flowrateWater1))
 
-    output = pd.Series(temp, index=rateFuel)
+        extra = C_Water * (waterTarget-Tco) * massflowWater
+        print (f'flow : {flowrateWater1}  |  main: {Q}  |  watts : {extra}' )
+
+    output = pd.Series(temp, index=rateWater)
     return output
 
 
 
-rad1 = radCalc(numPlates1, heatXA1, length1, width1, plateThk1, flowrateCoolant1)
-rad2 = radCalc(numPlates2, heatXA2, length2, width2, plateThk2, flowrateCoolant2)
-rad3 = radCalc(numPlates3, heatXA3, length3, width3, plateThk3, flowrateCoolant3)
+rad1 = radCalc(numPlates1, heatXA1, length1, width1, plateThk1, flowrateAF1)
+rad2 = radCalc(numPlates2, heatXA2, length2, width2, plateThk2, flowrateAF2)
+# rad3 = radCalc(numPlates3, heatXA3, length3, width3, plateThk3, flowrateAF3)
 #rad4 = radCalc(Height4, Width4, Thickness4, fanFlow4, speed4, tempAir4)
-#rad5 = radCalc(Height5, Width5, Thickness5, fanFlow5, flowrateCoolant5, tempAir5)
+#rad5 = radCalc(Height5, Width5, Thickness5, fanFlow5, flowrateAF5, tempAir5)
 
-dataSet = pd.DataFrame({'{0} Plate'.format(numPlates1)      : rad1,
-                        '{0} Plate'.format(numPlates2)      : rad2,
-                        '{0} Plate'.format(numPlates3)      : rad3})
-print dataSet
+dataSet = pd.DataFrame({f'{numPlates1} Plate, S'  : rad1,
+                        f'{numPlates2} Plate'     : rad2})#,
+                        # f'{numPlates3} Plate, L'  : rad3})
+
+                        # f'{numPlates1} Plate'  : rad1,
+# print (dataSet)
+
 
 dataSet.plot()
-plt.xlabel('GPM')
+plt.xlabel('LPM')
 plt.ylabel('Celcius')
 plt.title('Duda Diesel B3-14DW Comparison')
 ymin, ymax = plt.ylim()
-plt.text(0.01, ymin + 1, 'Th={0}, Tc={1}'.format(tempCoolant, tempFuel), size=8)
+plt.text(0.01, ymin + 1, 'Th={0}, Tc={1}'.format(tempAF, tempWater), size=8)
 plt.grid(1)
 plt.show()
 
